b634386b2d412d80bcecd248b5468edaf15fd6c0
benfallas <bsanchez@uber.com>
2022-06-18 16:16:25 -0700

    Drawing a Welcome message to the screen. Made screen background white

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 
 # import pygame package
 import pygame
+pygame.init()
+
+# define the RGB value for white,
+#  green, blue colour .
+white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 128)
+
+screenWidth = pygame.display.Info().current_w # gets monitor info
+screenHeight = pygame.display.Info().current_h # gets monitor info
 
 # initializing imported module
-pygame.init()
+font = pygame.font.Font('freesansbold.ttf', 32)
+
 
 # displaying a window of height
 # 500 and width 400
-pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+displaySurface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
 # creating a bool value which checks
 # if game is running
 running = True
+text = font.render("Welcome", True, green, blue)
+textRect = text.get_rect()
+textRect.center = (screenWidth / 2, screenHeight / 2)
 
 # keep game running till running is true
 while running:
+
+    #Make the screen white
+    displaySurface.fill(white)
+
+    # Draw the text over the rectangle
+    displaySurface.blit(text, textRect)
 
     # Check for event if user has pushed
     # any event in queue
@@ -24,7 +44,10 @@ while running:
         # set running bool to false
         if event.type == pygame.QUIT:
             running = False
+        # Draws the surface object to the screen.
+        pygame.display.update()
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
+                #If escame key pressed end the game
                 running = False
